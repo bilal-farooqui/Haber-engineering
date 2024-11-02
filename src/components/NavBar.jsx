be3398042754eea1b44677f37cf3cca5f15dc6de
Wasil0 <20wasil04@gmail.com>
2024-11-02 18:05:10 +0500

    done with navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,54 +1,35 @@
-import React, { useState } from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
-} from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Input } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Example(args) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
-  return (
-    <div>
-      <Navbar {...args}>
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-      </Navbar>
-    </div>
-  );
+function NavBar(args) {
+    return (
+        <div className="px-4">
+            <Navbar>
+                <NavbarBrand href="/">Haber-Engineering</NavbarBrand>
+                <Nav className="me-auto">
+                    <NavItem>
+                        <NavLink disabled href="/services/">
+                            Services
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink disabled href="/products/">
+                            Products
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink disabled href="/about/">
+                            About
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <div>
+                    <Input placeholder="search" />
+                </div>
+            </Navbar>
+        </div>
+    );
 }
 
-export default Example;
+export default NavBar;
