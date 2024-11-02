9bfb1ef463f424b3154556c3dc18a869e68b52b0
Wasil0 <20wasil04@gmail.com>
2024-11-03 02:17:24 +0500

    done with intro panel and service panel

---
+++ src/Constants/data.js
@@ -0,0 +1,27 @@
+export const introData ={
+    title:"Find appropriate solutions for your problems.",
+    description:"Discover the perfect power solutions for your needs with our extensive range of generator products and expert servicing.",
+    buttonText:"Shop Now",
+    image:require("./../assets/intropanelimg.jpg")
+}
+
+export const serviceData = [{
+    title: "Installation",
+    description: "Our technicians expertly install standby generators, ensuring proper integration with your electrical system and compliance with all regulations.",
+    buttonText: "Know More",
+    image: require("../assets/installation.jpg")
+    },
+    {
+        title: "Maintenance",
+        description: "We offer routine maintenance to keep your generator running smoothly, including oil changes and component checks",
+        buttonText: "Know More",
+        image: require("../assets/maintenance.jpg") 
+    },
+    {
+        title: "Inspection",
+    description: "Our thorough inspections ensure your generator is in optimal condition, identifying potential issues before they become problems.",
+    buttonText: "Know More",
+    image: require("../assets/inspection.jpg")
+    }
+]
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import IntroPanel from "../components/IntroPanel";
+import ServicePanel from "../components/ServicePanel";
 
 const Home = () =>{
 
     return(
         <>
         <NavBar/>
+        <IntroPanel/>
+        <ServicePanel/>
         </>
     )
 };
